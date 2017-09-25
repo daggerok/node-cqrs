f3c707c8d79fc8798ee2372a3ecb5720e42b0f78
Maksim Kostromin <daggerok@gmail.com>
2017-09-26 02:07:58 +0300

    finish some dirty command

--- message-command/index.js
+++ message-command/index.js
@@ -17,9 +17,35 @@ app.get('/', (req, res) => {
   });
 });
 
+const events = {
+  types: {
+    messages: {
+      create: 'messages.create',
+    },
+  },
+};
+
+const busHost = process.env.RABBITMQ_HOST || '127.0.0.1';
+const busPort = process.env.RABBITMQ_PORT || '5672';
+const url =  process.env.RABBITMQ_URL || `amqp://${busHost}:${busPort}`;
+const bus = require('servicebus').bus({ url });
+
+bus.listen(events.types.messages.create, e => {
+  console.log('message created', e);
+});
+
 app.post('/api/v1/message', (req, res) => {
   const message = req.body;
-  res.send('Hello World!');
+  const payload = {
+    id: Date.now(),
+    message,
+  };
+  bus.send(events.types.messages.create, {
+    payload,
+  });
+  res.json({
+    message: 'message sent',
+  });
 });
 
 const port = process.env.MESSANGER_PORT || process.env.PORT || 3000;
